236c1b717406895c972aaeed6c25d8c5834e2224
nitesh bhardwaj <niteshbhardwaj9313@gmail.com>
2024-05-18 20:05:24 +0530

    optional api and host

--- src/plugins/config.ts
+++ src/plugins/config.ts
@@ -15,8 +15,8 @@ const ConfigSchema = Type.Strict(
   Type.Object({
     NODE_ENV: Type.Enum(NodeEnv),
     LOG_LEVEL: Type.String(),
-    HOST: Type.String().Optional(),
-    PORT: Type.Number().Optional(),
+    // HOST: Type.String().Optional(),
+    // PORT: Type.Number().Optional(),
     DATABASE_URL: Type.String(),
   })
 );
